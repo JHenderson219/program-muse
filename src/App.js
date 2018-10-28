db7b9ebac24038c0c5394dc206158ff47a691c81
Jordan Henderson <hendersonj219@gmail.com>
2018-10-27 17:41:04 -0700

    Basic testing get request to YT API

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import config from './config.js';
+// import search from './../node_modules/youtube-search';
+
 
 class App extends Component {
+  getData() {
+    fetch(`${config.YT_DATA_URL}key=${config.YT_DATA_KEY}&part=snippet&q=synthwave&maxResults=20`).then((resp)=>{
+      resp.json().then((json) => {console.log(json)});
+    });
+  }
   render() {
+    this.getData();
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            {config.YT_DATA_URL+config.YT_DATA_KEY}
           </p>
           <a
             className="App-link"
